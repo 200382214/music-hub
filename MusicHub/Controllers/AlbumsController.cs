85a57484e47aa35f17b6a424419e8c5c54cdb443
Dhvanit Merchant <200382214@student.georgianc.on.ca>
2018-09-21 13:34:09 -0400

    code before github

---
+++ MusicHub/Controllers/AlbumsController.cs
@@ -0,0 +1,33 @@
+﻿
+using MusicHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicHub.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        // GET: Albums
+        public ActionResult Index()
+        {
+
+
+            // create a mock list of type Albums
+            var albums = new List<Album>();
+
+            for(int i =1; i <= 10; i++)
+            {
+                albums.Add(new Album { Title = "Album " + i.ToString() });
+            }
+
+            //add albums to viewbag for display
+            //ViewBag.albums = albums;
+
+            //pass the stongly typed list directly to the view
+            return View(albums);
+        }
+    }
+}
